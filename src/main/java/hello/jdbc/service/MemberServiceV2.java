c3fa7b8a2bf1bc00130ed5278a9b9bba409c130b
sangjin <insangjin6439@gmail.com>
2024-06-05 00:29:54 +0900

    DB락 변경,조회, 트랜젝션 적용

---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,31 @@
+package hello.jdbc.service;
+
+import java.sql.SQLException;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney()-money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney()+money);
+
+    }
+
+    private void validation(final Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+
+}
